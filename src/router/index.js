3422bc4e5c296022e541daf6c12bd3fb0221eb70
Akira <kuramal12@msn.com>
2022-02-26 15:45:44 +0800

    Adding Route Page Components

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+// #region 頁面
+import CoachDetail from '../views/coaches/CoachDetail.vue';
+import CoachesList from '../views/coaches/CoachesList.vue';
+import CoachRegistration from '../views/coaches/CoachRegistration.vue';
+import ContactCoach from '../views/requests/ContactCoach.vue';
+import RequestsReceive from '../views/requests/RequestsReceive.vue';
+import NotFound from '../views/NotFound.vue';
+// #endregion
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', redirect: '/coaches'},
+        { path: '/coaches', component: CoachesList },
+        { 
+            path: '/coaaches/:id',
+            component: CoachDetail,
+            children: [
+                { path: 'contact', component: ContactCoach }
+            ]
+        },
+        { path: '/register', component: CoachRegistration },
+        { path: '/requests', component: RequestsReceive },
+        { path: '/:notFound(.*)', component: NotFound }
+    ]
+});
+
+export default router;
---
+++ src/views/NotFound.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>Oops, something error</h2>
+</template>
