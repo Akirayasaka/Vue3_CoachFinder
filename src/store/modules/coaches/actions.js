8ab49f82de7eac0f3e860afbc69f7f9fed2bc0e4
Akira <kuramal12@msn.com>
2022-03-01 15:30:02 +0800

    Use http Request to store data
    FireBase: realtime database & developer mode

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,7 +1,7 @@
 export default {
-    registerCoach(context, data){
+    async registerCoach(context, data){
+        const userId = context.rootGetters.getUserId;
         const coachData = {
-            id: context.rootGetters.getUserId,
             firstName: data.firstName,
             lastName: data.lastName,
             description: data.description,
@@ -9,6 +9,22 @@ export default {
             areas: data.areas
         };
 
-        context.commit('mutRegisterCoach', coachData);
+        // Use FireBase for backend
+        const response = await fetch(`https://vue-http-demo-82225-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
+            method: 'PUT',
+            body: JSON.stringify(coachData)
+        });
+
+        //const responseData = await response.json();
+
+        if(!response.ok) {
+            //error ..
+        }
+
+        //context.commit('mutRegisterCoach', coachData);
+        context.commit('mutRegisterCoach', {
+            ...coachData,
+            id: userId
+        });
     }
 };
