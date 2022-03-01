2fe50abee10283af2b6e7831e906fac8c6f8158e
Akira <kuramal12@msn.com>
2022-03-01 11:41:03 +0800

    Working on the Contact Form

--- src/views/requests/ContactCoach.vue
+++ src/views/requests/ContactCoach.vue
@@ -1,3 +1,95 @@
 <template>
-    Contact a Coach
+    <form>
+        <div class="form-control">
+            <label for="email">Your E-Mail</label>
+            <input type="email" id="email" v-model.trim="model.formData.email" />
+        </div>
+        <div class="form-control">
+            <label for="message">Message</label>
+            <textarea id="message" rows="5" v-model.trim="model.formData.message" ></textarea>
+        </div>
+        <p class="errors" v-if="!model.formData.formIsValid">Please enter a valid email and non-empty message.</p>
+        <div class="actions">
+            <base-button @click.prevent="submitForm">Send Message</base-button>
+        </div>
+    </form>
 </template>
+
+<script>
+import { reactive } from '@vue/reactivity';
+
+export default {
+    setup() {
+        const model = reactive({
+            formData: {
+                email: '',
+                message: '',
+                formIsValid: true
+            }
+        });
+
+        const submitForm = () => {
+            model.formData.formIsValid = true;
+            if(model.formData.email === '' || !model.formData.email.includes('@') || model.formData.message === '' ){
+                model.formData.formIsValid = false;
+                return;
+            }
+            
+            const dto = {
+                email: model.formData.email,
+                message: model.formData.message
+            }
+            console.log(dto);
+        }
+
+        return {
+            model,
+            submitForm
+        }
+    },
+}
+</script>
+
+<style scoped>
+form {
+  margin: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+
+.errors {
+  font-weight: bold;
+  color: red;
+}
+
+.actions {
+  text-align: center;
+}
+</style>
