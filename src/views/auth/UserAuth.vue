fd125226de07d2b49a1217827e14bd953fbd3877
Akira <kuramal12@msn.com>
2022-03-02 11:30:31 +0800

    Auth Page: add Loading Spinner & Error Handling

--- src/views/auth/UserAuth.vue
+++ src/views/auth/UserAuth.vue
@@ -1,20 +1,28 @@
 <template>
-  <base-card>
-    <form>
-      <div class="form-contorl">
-        <label for="email">E-mail</label>
-        <input type="email" name="" id="email" v-model.trim="model.formData.email" />
-      </div>
-      <div class="form-contorl">
-        <label for="password">Password</label>
-        <input type="password" name="" id="password" v-model.trim="model.formData.password" />
-      </div>
-      <br/>
-      <p v-if="!model.formIsValid">Please enter a valid email and password(must be at least 6 characters long.)</p>
-      <base-button @click.prevent="submitForm">{{submitButtonCaption}}</base-button>
-      <base-button type="button" mode="flat" @click.prevent="switchAuthMode">{{switchModeButtonCaption}}</base-button>
-    </form>
-  </base-card>
+  <div>
+    <base-dialog :show="!!model.error" title="An error occured" @close="handlerError">
+      <p>{{ model.error }}</p>
+    </base-dialog>
+    <base-dialog :show="model.isLoading" title="Authenticating..." fixed>
+      <base-spinner></base-spinner>
+    </base-dialog>
+    <base-card>
+      <form>
+        <div class="form-contorl">
+          <label for="email">E-mail</label>
+          <input type="email" id="email" v-model.trim="model.formData.email" />
+        </div>
+        <div class="form-contorl">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model.trim="model.formData.password" />
+        </div>
+        <br />
+        <p v-if="!model.formIsValid">Please enter a valid email and password(must be at least 6 characters long.)</p>
+        <base-button @click.prevent="submitForm">{{ submitButtonCaption }}</base-button>
+        <base-button type="button" mode="flat" @click.prevent="switchAuthMode">{{ switchModeButtonCaption }}</base-button>
+      </form>
+    </base-card>
+  </div>
 </template>
 
 <script>
@@ -27,57 +35,76 @@ export default {
     const model = reactive({
       formData: {
         email: '',
-        password: ''
+        password: '',
       },
       formIsValid: true,
-      mode: 'login'
+      mode: 'login',
+      isLoading: false,
+      error: null,
     });
     const $store = useStore();
 
-    const submitForm = () => {
+    const submitForm = async () => {
       model.formIsValid = true;
-      if(model.formData.email === '' || !model.formData.email.includes('@') || model.formData.password.length < 6){
+      if (
+        model.formData.email === '' ||
+        !model.formData.email.includes('@') ||
+        model.formData.password.length < 6
+      ) {
         model.formIsValid = false;
         return;
       }
+
+      model.isLoading = true;
+
       // send http request
-      if(model.mode === 'login'){
-        console.log(model.formData);
-      }else{
-        // vuex auth沒有namespaced, 可直接呼叫
-        $store.dispatch('signup', model.formData);
+      try {
+        if (model.mode === 'login') {
+          console.log(model.formData);
+        } else {
+          // vuex auth沒有namespaced, 可直接呼叫
+          await $store.dispatch('signup', model.formData);
+        }
+      } catch (err) {
+        model.error = err.message || 'Failed to authenticate, try later.';
       }
+
+      model.isLoading = false;
     };
-    const switchAuthMode = () =>{
-      if(model.mode === 'login'){
+    const switchAuthMode = () => {
+      if (model.mode === 'login') {
         model.mode = 'signup';
-      }else{
+      } else {
         model.mode = 'login';
       }
-    }
+    };
+    const handlerError = () => {
+      model.error = null;
+    };
 
-    const submitButtonCaption = computed(()=>{
-      if(model.mode === 'login'){
+    const submitButtonCaption = computed(() => {
+      if (model.mode === 'login') {
         return 'Login';
-      }else{
+      } else {
         return 'Signup';
       }
     });
-    const switchModeButtonCaption = computed(()=>{
-      if(model.mode === 'login'){
+    const switchModeButtonCaption = computed(() => {
+      if (model.mode === 'login') {
         return 'Signup instead';
-      }else{
+      } else {
         return 'Login instead';
       }
     });
 
     return {
       model,
+      handlerError,
       submitForm,
       switchAuthMode,
       submitButtonCaption,
-      switchModeButtonCaption
-    }
+      switchModeButtonCaption,
+    };
   },
 };
 </script>
